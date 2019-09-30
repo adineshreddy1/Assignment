592f96649a901ca3e772ba24a20c9200718864de
Dinesh <dinesh@Dineshs-MacBook-Pro.local>
2019-10-01 00:01:36 +0530

    channel

--- network/generate.sh
+++ network/generate.sh
@@ -1,2 +1,10 @@
 ./bin/cryptogen generate --config=./crypto-config.yaml
 
+
+
+mkdir channel-artifacts
+
+./bin/configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block 
+./bin/configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID token-transfer-channel 
+./bin/configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/iciciMSPanchors.tx -channelID  token-transfer-channel -asOrg iciciMSP
+./bin/configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/sbiMSPanchors.tx -channelID  token-transfer-channel -asOrg sbiMSP
---
+++ network/remove.sh
@@ -0,0 +1,3 @@
+
+rm -rf crypto-config
+rm -rf channel-artifacts
