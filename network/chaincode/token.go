c4babd9b99fcaf3e8cdd8cc4e7a18961a6426a80
Dinesh <dinesh@Dineshs-MacBook-Pro.local>
2019-10-01 14:10:00 +0530

    token.go

--- network/chaincode/token.go
+++ network/chaincode/token.go
@@ -9,6 +9,8 @@ import (
 // Tokentrans type
 type Tokentrans struct {
 	Name  string `json:"Name"`
+	Id string `json:"Id"`
+	Company string `json:"Company"`
 	Token string `json:"Token"`
 }
 
@@ -21,18 +23,36 @@ func main() {
 		fmt.Println("Chaincode installed successfully")
 	}
 }
+
+func GenerateToken(Name string, Id string, Company string) string {
+	
+	//Generate Token for shared License
+	tokenString := fmt.Sprintf("%s%s%d%s", Name, Id, Company, time.Now().String())
+	input := strings.NewReader(tokenString)
+	hash := sha256.New()
+	if _, err := io.Copy(hash, input); err != nil {
+		fmt.Println("Unable to Generate Token in GenerateToken: ", err)
+		return string(err.Error())
+	}
+
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
 //Init Tokentrans  comment
 func (t *Tokentrans) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	fun, args := stub.GetFunctionAndParameters()
 fmt.Println("Arguements ", func)
+
 switch fun {
-case "InitMarble":
+case "GenerateToken":
 	//create a new token
-	return t.initMarble(stub, args)
+	return t.GenerateToken(stub, args)
 case "TransferToken":
 	//change owner of a specific marble
-	return t.transferToken(stub, args)
+	return t.TransferToken(stub, args)
 case "queryName":
 	//find marbles based on an ad hoc rich query
 	return t.queryName(stub, args)
+
+	}
 }
