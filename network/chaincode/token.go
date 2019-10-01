d84ddac9ff40272acabe7eb9f279ffbb6b4a5fa4
adineshreddy1 <adineshreddy1@gmail.com>
2019-10-01 16:24:56 +0530

    func for tokem

--- network/chaincode/token.go
+++ network/chaincode/token.go
@@ -1,22 +1,30 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io"
+	"strings"
+	"time"
+
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
 // Tokentrans type
 type Tokentrans struct {
-	Name  string `json:"Name"`
-	Id string `json:"Id"`
+	Name    string `json:"Name"`
+	ID      string `json:"ID"`
 	Company string `json:"Company"`
-	Token string `json:"Token"`
+	Token   string `json:"Token"`
 }
 
 func main() {
 
-	err := shim.Start(new(Token))
+	err := shim.Start(new(Tokentrans))
 	if err != nil {
 		fmt.Println("Error with chaincode")
 	} else {
@@ -24,10 +32,39 @@ func main() {
 	}
 }
 
-func GenerateToken(Name string, Id string, Company string) string {
-	
-	//Generate Token for shared License
-	tokenString := fmt.Sprintf("%s%s%d%s", Name, Id, Company, time.Now().String())
+//Init Tokentrans  comment
+func (t *Tokentrans) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("Initiate the chaincode")
+	return shim.Success(nil)
+}
+
+//Invoke Tokentrans  comment
+func (t *Tokentrans) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	fun, args := stub.GetFunctionAndParameters()
+	fmt.Println("Arguements ", fun)
+
+	switch fun {
+	case "GenerateToken":
+		//create a new token
+		return t.GenerateToken(stub, args)
+	case "TransferToken":
+		//change owner of a specific token
+		return t.TransferToken(stub, args)
+	case "GetAllToken":
+		//find marbles based on an ad hoc rich query
+		return t.GetAllToken(stub, args)
+
+	}
+
+	fmt.Println("Function not found!")
+	return shim.Error("Recieved unknown function invocation!")
+}
+
+//GenToken for
+func GenToken(Name string, ID string, Company string) string {
+
+	//Generate Token for
+	tokenString := fmt.Sprintf("%s%s%d%s", Name, ID, Company, time.Now().String())
 	input := strings.NewReader(tokenString)
 	hash := sha256.New()
 	if _, err := io.Copy(hash, input); err != nil {
@@ -38,21 +75,106 @@ func GenerateToken(Name string, Id string, Company string) string {
 	return hex.EncodeToString(hash.Sum(nil))
 }
 
-//Init Tokentrans  comment
-func (t *Tokentrans) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fun, args := stub.GetFunctionAndParameters()
-fmt.Println("Arguements ", func)
+//GenerateToken for user
+func (t *Tokentrans) GenerateToken(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-switch fun {
-case "GenerateToken":
-	//create a new token
-	return t.GenerateToken(stub, args)
-case "TransferToken":
-	//change owner of a specific marble
-	return t.TransferToken(stub, args)
-case "queryName":
-	//find marbles based on an ad hoc rich query
-	return t.queryName(stub, args)
+	var err error
+	var tokenobj Tokentrans
 
+	if len(args) < 1 {
+		fmt.Println("Invalid number of arguments")
+		return shim.Error(err.Error())
 	}
+
+	err = json.Unmarshal([]byte(args[0]), &tokenobj)
+	if err != nil {
+		fmt.Println("Unable to unmarshal data in Generatetoken : ", err)
+		return shim.Error(err.Error())
+	}
+	tokenobj.Token = GenToken(tokenobj.Name, tokenobj.ID, tokenobj.Company)
+
+	JSONBytes1, err1 := json.Marshal(tokenobj)
+	if err1 != nil {
+		fmt.Println("Unable to Marshal Generatetoken: ", err1)
+		return shim.Error(err1.Error())
+	}
+
+	err = stub.PutState(tokenobj.Token, JSONBytes1)
+	// End - Put into Couch DB
+	if err != nil {
+		fmt.Println("Unable to make transaction for Generatetoken: ", err)
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
 }
+
+//TransferToken token need to give name to transfer and token generated
+func (t *Tokentrans) TransferToken(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) < 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
+	}
+
+}
+
+//GetAllToken func
+func (t *Tokentrans) GetAllToken(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var err error
+
+	queryString := fmt.Sprintf("{\"selector\":{\"RootLcToken\":{\"$ne\": \"%s\"}}}", "null")
+	queryResults, err := getQueryResultForQueryString(stub, queryString)
+	//fetch data from couch db ends here
+	if err != nil {
+		fmt.Printf("Unable to get All Token details: %s\n", err)
+		return shim.Error(err.Error())
+	}
+	fmt.Printf("Token Details : %v\n", queryResults)
+
+	return shim.Success(queryResults)
+}
+
+// getQueryResultForQueryString
+func getQueryResultForQueryString(stub shim.ChaincodeStubInterface, queryString string) ([]byte, error) {
+
+	fmt.Printf("***getQueryResultForQueryString queryString:\n%s\n", queryString)
+
+	resultsIterator, err := stub.GetQueryResult(queryString)
+
+	if err != nil {
+		fmt.Println("Error from getQueryResultForQueryString:  ", err)
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing QueryRecords
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("***getQueryResultForQueryString queryResult:\n%s\n", buffer.String())
+
+	return buffer.Bytes(), nil
+} // getQueryResultForQueryString
